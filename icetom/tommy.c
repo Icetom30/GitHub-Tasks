8e33ad1b42b7ae1825883b63214373a529910e0f
Tswain <isaactom55@gmail.com>
2022-04-17 14:57:32 +0100

    Double and float variable

---
+++ icetom/tommy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point prints Double and Float Variables
+ *
+ * Return: Always 0(Success)
+ */
+int main()
+{
+        double number1 = 25.34;
+        float number2 = 45.0f;
+
+        printf("number1: %1f", number1);
+        printf("\n");
+        printf("number2: %f", number2);
+        return 0;
+}
